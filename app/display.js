7e8ab592fa544cd6c84179f784712df2c50a4063
Thor Kell <thor@tide-pool.ca>
2022-02-13 17:54:14 -0500

    fix cursor

--- app/display.js
+++ app/display.js
@@ -111,5 +111,6 @@ fetch('vinyl.json')
   .then(response => response.json())
   .then(data => {
       var canvas = new fabric.Canvas('vinylCanvas');
+      canvas.hoverCursor = 'default';
       tess.render(canvas, tess.prepare(data))
   });
