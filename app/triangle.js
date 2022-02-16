747b802f07ee63e108468e350351ca5013b1d90c
Thor Kell <thor@tide-pool.ca>
2022-02-15 22:41:59 -0500

    wip, got a click and fixed triangle rotation chaos

--- app/triangle.js
+++ app/triangle.js
@@ -30,13 +30,14 @@ makeTriangle = function () {
   
   triangle.render = function(c, data) {
     for (let record of data) {
+      // this can be extracted to be one "up" clippath  and one "down" path!
       let triangleClipPath = new fabric.Triangle({
         originX: 'center',
         originY: 'center',
         width: this.xSize,
         height: this.ySize,
         angle: record.angle,
-        selectable: false,
+        selectable: false
       });
       let imagePath = "images/" + record.id + ".jpg"
       fabric.Image.fromURL(imagePath, function(img) {
@@ -45,7 +46,36 @@ makeTriangle = function () {
         img.selectable = false;
         img.clipPath = triangleClipPath;
         c.add(img);
+        c.sendToBack(img);
       });
+      console.log(record.x, record.y, record.angle);
+      // hack to fix angle hell  
+      // I will do this all in the prepare script, I think - same for the image centering tweak, if I can?
+      if (record.angle == 180) {
+          record['fixedY'] = record.y + triangle.ySize;
+          record['fixedX'] = record.x + triangle.xSize;
+      } else {
+          record['fixedY'] = record.y;
+          record['fixedX'] = record.x;
+      }
+
+      let triangleToClick = new fabric.Triangle({
+        left: record.fixedX,
+        top: record.fixedY,
+        fill: 'rgba(0,0,0,0)',
+        width: this.xSize,
+        height: this.ySize,
+        angle: record.angle,
+        selectable: false
+      });
+      triangleToClick.on('mouseup', function(options) {
+        // need to fix the CSS to make it not move the canvas around  !
+        console.log(record);
+        var t = document.getElementById('text');
+        t.textContent = record.title;
+      });
+      c.add(triangleToClick);
+      c.bringToFront(triangleToClick);
     }
   }
   return triangle;
