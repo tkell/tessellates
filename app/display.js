508004c5729e9cd45203337588116bd2a7ffcec3
Thor Kell <thor@tide-pool.ca>
2022-02-16 21:38:47 -0500

    paging with re-draws, probably a better way but fine for now

--- app/display.js
+++ app/display.js
@@ -1,20 +1,38 @@
 let params = getSearchParameters();
 let parsedParams = parseTessellatesParams(params)
-//
 // pick a tessellation, then ..
 if (parsedParams['t'] == 'square') {
-    tess = makeSquare();
+  tess = makeSquare();
 } else {
-    tess = makeTriangle();
+  tess = makeTriangle();
+}
+var vinylData = [];
+var canvas = null;
+
+function renderCanvas(canvas, tess, data, params) {
+    canvas.clear();
+    offset = params['offset'];
+    end = offset + params['items'];
+    slicedData = data.slice(offset, end);
+    tess.render(canvas, tess.prepare(slicedData))
 }
 
 fetch('vinyl.json')
   .then(response => response.json())
   .then(data => {
-      offset = params['offset'];
-      end = offset + params['items'];
-      data = data.slice(offset, end);
-      var canvas = new fabric.Canvas('vinylCanvas');
-      canvas.hoverCursor = 'default';
-      tess.render(canvas, tess.prepare(data))
+    canvas = new fabric.Canvas('vinylCanvas');
+    canvas.hoverCursor = 'default';
+    vinylData = data;
+    renderCanvas(canvas, tess, vinylData, params)
   });
+
+window.onload = function(event) {
+  document.getElementById('back').addEventListener("click", function(e) {
+    params['offset'] = Math.max(0, params['offset'] - 10)
+    renderCanvas(canvas, tess, vinylData, params);
+  })
+  document.getElementById('forward').addEventListener("click", function(e) {
+    params['offset'] = Math.min(vinylData.length, params['offset'] + 10)
+    renderCanvas(canvas, tess, vinylData, params);
+  })
+};
