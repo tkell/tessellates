1046798ded5223d66b14ff6df670c900dc031a86
Thor Kell <thor@tide-pool.ca>
2022-03-08 19:56:05 -0500

    refactor animation to own file

--- app/tessellation-helpers.js
+++ app/tessellation-helpers.js
@@ -60,43 +60,6 @@ function clearImageFilters(record) {
   }
 }
 
-/*
- Some complexity here, as Fabric is not great at animation chains:
- We take a list of animations, in the order we want them to happen:  [go left, then go right]
- We make our "finisher", finishAnimation
- We reverse the animations
- Then, for each animation, we set it up to onComplete the "previous" animation –
- which is actually the _next_ animation, because we reversed it!
- We continue to connect the previous animation to the next via onComplete ...
- After we've done everything, we return the "first" animation function, so we can run it!
-*/ 
-function setupAnimationChain(record, animations) {
-  let finishAnimation = function () {
-    record.image.clipPath = record.clipPath;
-    canvas.insertAt(record.image);
-    record.isAnimating = false;
-  }
-
-  let backwardsAnimations = animations.reverse();
-  let animationFunctions = [finishAnimation];
-  for (let i=0; i < animations.length; i++) {
-    let animation = animations[i];
-    let scopedOnComplete = animationFunctions[animationFunctions.length -1];
-    let a = function() {
-      let options = {
-        onChange: canvas.renderAll.bind(canvas),
-        duration: animation.duration,
-        onComplete: scopedOnComplete
-      }
-      record.isAnimating = true;
-      record.image.animate(animation.target, animation.change, options)
-    }
-
-    animationFunctions.push(a);
-  }
-
-  return animationFunctions[animationFunctions.length - 1];
-}
 
 tessellationHelper.createDefaultClickState = function (canvas, record, data) {
   let objectToClick = record.clickable;
@@ -115,7 +78,7 @@ tessellationHelper.createDefaultClickState = function (canvas, record, data) {
     }
   });
 
-  let bounce = setupAnimationChain(record, [
+  let bounce = animationHelper.setupAnimationChain(record, [
     {target: 'left', change: '+=10', duration: 100},
     {target: 'left', change: '-=15', duration: 125},
     {target: 'left', change: '+=5', duration: 50},
