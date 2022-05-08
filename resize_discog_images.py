20146198936f90017211402bcf8f23181b12f585
Thor Kell <thor@tide-pool.ca>
2022-05-08 16:19:37 -0400

    sketch for resizing images

---
+++ resize_discog_images.py
@@ -0,0 +1,19 @@
+import os
+from PIL import Image
+
+small_size = (350, 350)
+image_dir = "app/images"
+for filename in os.listdir(image_dir):
+    if ".jpg" in filename:
+        print(filename)
+        record_id = filename.split(".")[0]
+        outfile = image_dir + os.path.sep + f"{record_id}-small.jpg"
+        if os.path.isfile(outfile):
+            continue
+
+        infile = image_dir + os.path.sep + filename
+        outfile = image_dir + os.path.sep + f"{record_id}-small.jpg"
+        with Image.open(infile) as im:
+            im.thumbnail(small_size)
+            im.save(outfile, "JPEG")
+        print(im.format, im.size, im.mode)
