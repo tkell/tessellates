8133b521436c65717663146e021a959ea2987bbd
Thor Kell <thor@tide-pool.ca>
2025-10-14 19:35:42 -0400

    remove unused CSS test

--- new-css-test/new-index.js
+++
@@ -1,8 +0,0 @@
-// entry
-window.addEventListener("load", (event) => {
-  const ourImage = document.getElementById("test");
-  ourImage.addEventListener("click", function(e) {
-    ourImage.classList.add("motion-demo");
-    console.log("hello");
-  })
-})
